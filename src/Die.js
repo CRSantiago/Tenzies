56f6928ce822b268e179ed0ccf5db1f2b7f14c2b
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2022-06-02 11:02:35 -0400

    Dice face addition

--- src/Die.js
+++ src/Die.js
@@ -1,16 +1,27 @@
 import React from "react"
+import "./die.css";
+
+const Pip = () => <span className="pip"/>;
+
+const Face = ({ children , style}) => <div className="face" style={style}>{children}</div>;
 
 export default function Die(props) {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
+        backgroundColor: props.isHeld ? "#59E391" : "#e7e7e7",
+        boxShadow: props.isHeld ? 'inset 0 5px #59E391, inset 0 -5px #59E391, inset 5px 0 #59E391,inset -5px 0 #59E391' : 'inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7,inset -5px 0 #d7d7d7' 
     }
+
+    let pips = Number.isInteger(props.value)
+		? Array(props.value)
+				.fill(0)
+				.map((_, i) => <Pip key={i} />)
+		: null;
     return (
         <div 
             className="die-face" 
-            style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
+            <Face style={styles}>{pips}</Face>
         </div>
     )
 }
